3269536571de4f2ba2c947a008b8b7bcefc09369
Мария Малькова <Tyuiu.MalkovaMS@outlook.com>
2025-12-14 13:04:04 +0500

    Added test and library

--- Tyuiu.MalkovaMS.Sprint7.Project.V7.Lib/DataService.cs
+++ Tyuiu.MalkovaMS.Sprint7.Project.V7.Lib/DataService.cs
@@ -2,6 +2,23 @@
 {
     public class DataService
     {
-
+        public string[,] LoadFromFileData(string filePath)
+        {
+            string fileData = File.ReadAllText(filePath);
+            fileData = fileData.Replace('\n', '\r');
+            string[] lines = fileData.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int rows = lines.Length;
+            int cols = lines[0].Split(';').Length;
+            string[,] arrayValues = new string[rows, cols];
+            for (int i = 0; i < rows; i++)
+            {
+                string[] line = lines[i].Split(';');
+                for (int j = 0; j < cols; j++)
+                {
+                    arrayValues[i, j] = Convert.ToString(line[j]);
+                }
+            }
+            return arrayValues;
+        }
     }
 }
--- Tyuiu.MalkovaMS.Sprint7.Project.V7.Test/DataServiceTest.cs
+++ Tyuiu.MalkovaMS.Sprint7.Project.V7.Test/DataServiceTest.cs
@@ -1,11 +1,26 @@
-﻿namespace Tyuiu.MalkovaMS.Sprint7.Project.V7.Test
+﻿using Tyuiu.MalkovaMS.Sprint7.Project.V7.Lib;
+namespace Tyuiu.MalkovaMS.Sprint7.Project.V7.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void FileExist()
         {
+            string path = @"C:\Users\marim\source\repos\Tyuiu.MalkovaMS.Sprint7\Tyuiu.MalkovaMS.Sprint7.Project.V7\dom2.csv";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+
+        [TestMethod]
+        public void ValidLoadFromFileData()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint7\test1.txt";
+            string[,] wait = new string[,] { { "1", "5", "dum" }, { "tiu", "15", "ok" } };
+            CollectionAssert.AreEqual(wait, ds.LoadFromFileData(path));
         }
     }
 }
