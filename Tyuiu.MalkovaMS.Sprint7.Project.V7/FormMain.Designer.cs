a0d9b7b39a0ddc4be3135711e7d8e7fc54443560
MalkovaMS <mari.malkova.2007>
2025-12-18 23:31:12 +0500

    Edited FormMain

--- Tyuiu.MalkovaMS.Sprint7.Project.V7/FormMain.Designer.cs
+++ Tyuiu.MalkovaMS.Sprint7.Project.V7/FormMain.Designer.cs
@@ -28,19 +28,194 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tabControlMain_MMS = new TabControl();
+            tabPageBase_MMS = new TabPage();
+            tabPageStat_MMS = new TabPage();
+            tabPageChart_MMS = new TabPage();
+            tabPagePayInfo_MMS = new TabPage();
+            tabPageAbout_MMS = new TabPage();
+            menuStripMain_MMS = new MenuStrip();
+            файлToolStripMenuItem_MMS = new ToolStripMenuItem();
+            справкаToolStripMenuItem_MMS = new ToolStripMenuItem();
+            загрузитьДанныеToolStripMenuItem_MMS = new ToolStripMenuItem();
+            сохранитьДанныеToolStripMenuItem_MMS = new ToolStripMenuItem();
+            выходToolStripMenuItem_MMS = new ToolStripMenuItem();
+            оПрограммеToolStripMenuItem_MMS = new ToolStripMenuItem();
+            руководствоПользователяToolStripMenuItem_MMS = new ToolStripMenuItem();
+            видToolStripMenuItem_MMS = new ToolStripMenuItem();
+            сменитьТемуToolStripMenuItem_MMS = new ToolStripMenuItem();
+            tabControlMain_MMS.SuspendLayout();
+            menuStripMain_MMS.SuspendLayout();
             SuspendLayout();
             // 
-            // Form1
+            // tabControlMain_MMS
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            tabControlMain_MMS.Controls.Add(tabPageBase_MMS);
+            tabControlMain_MMS.Controls.Add(tabPageStat_MMS);
+            tabControlMain_MMS.Controls.Add(tabPageChart_MMS);
+            tabControlMain_MMS.Controls.Add(tabPagePayInfo_MMS);
+            tabControlMain_MMS.Controls.Add(tabPageAbout_MMS);
+            tabControlMain_MMS.Location = new Point(7, 23);
+            tabControlMain_MMS.Margin = new Padding(4, 4, 4, 4);
+            tabControlMain_MMS.Name = "tabControlMain_MMS";
+            tabControlMain_MMS.SelectedIndex = 0;
+            tabControlMain_MMS.Size = new Size(1318, 638);
+            tabControlMain_MMS.TabIndex = 0;
+            // 
+            // tabPageBase_MMS
+            // 
+            tabPageBase_MMS.Font = new Font("Adobe Fan Heiti Std B", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            tabPageBase_MMS.Location = new Point(4, 29);
+            tabPageBase_MMS.Margin = new Padding(4, 4, 4, 4);
+            tabPageBase_MMS.Name = "tabPageBase_MMS";
+            tabPageBase_MMS.Padding = new Padding(4, 4, 4, 4);
+            tabPageBase_MMS.Size = new Size(1310, 605);
+            tabPageBase_MMS.TabIndex = 0;
+            tabPageBase_MMS.Text = "База данных";
+            tabPageBase_MMS.UseVisualStyleBackColor = true;
+            // 
+            // tabPageStat_MMS
+            // 
+            tabPageStat_MMS.Location = new Point(4, 29);
+            tabPageStat_MMS.Margin = new Padding(4, 4, 4, 4);
+            tabPageStat_MMS.Name = "tabPageStat_MMS";
+            tabPageStat_MMS.Padding = new Padding(4, 4, 4, 4);
+            tabPageStat_MMS.Size = new Size(1310, 605);
+            tabPageStat_MMS.TabIndex = 1;
+            tabPageStat_MMS.Text = "Статистика";
+            tabPageStat_MMS.UseVisualStyleBackColor = true;
+            // 
+            // tabPageChart_MMS
+            // 
+            tabPageChart_MMS.Location = new Point(4, 29);
+            tabPageChart_MMS.Margin = new Padding(4, 4, 4, 4);
+            tabPageChart_MMS.Name = "tabPageChart_MMS";
+            tabPageChart_MMS.Padding = new Padding(4, 4, 4, 4);
+            tabPageChart_MMS.Size = new Size(1310, 605);
+            tabPageChart_MMS.TabIndex = 2;
+            tabPageChart_MMS.Text = "Графики";
+            tabPageChart_MMS.UseVisualStyleBackColor = true;
+            // 
+            // tabPagePayInfo_MMS
+            // 
+            tabPagePayInfo_MMS.Location = new Point(4, 29);
+            tabPagePayInfo_MMS.Name = "tabPagePayInfo_MMS";
+            tabPagePayInfo_MMS.Size = new Size(1310, 605);
+            tabPagePayInfo_MMS.TabIndex = 3;
+            tabPagePayInfo_MMS.Text = "Информация о платежах";
+            tabPagePayInfo_MMS.UseVisualStyleBackColor = true;
+            // 
+            // tabPageAbout_MMS
+            // 
+            tabPageAbout_MMS.Location = new Point(4, 29);
+            tabPageAbout_MMS.Name = "tabPageAbout_MMS";
+            tabPageAbout_MMS.Size = new Size(1310, 605);
+            tabPageAbout_MMS.TabIndex = 4;
+            tabPageAbout_MMS.Text = "Справка";
+            tabPageAbout_MMS.UseVisualStyleBackColor = true;
+            // 
+            // menuStripMain_MMS
+            // 
+            menuStripMain_MMS.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem_MMS, справкаToolStripMenuItem_MMS, видToolStripMenuItem_MMS });
+            menuStripMain_MMS.Location = new Point(0, 0);
+            menuStripMain_MMS.Name = "menuStripMain_MMS";
+            menuStripMain_MMS.Size = new Size(1338, 24);
+            menuStripMain_MMS.TabIndex = 1;
+            // 
+            // файлToolStripMenuItem_MMS
+            // 
+            файлToolStripMenuItem_MMS.DropDownItems.AddRange(new ToolStripItem[] { загрузитьДанныеToolStripMenuItem_MMS, сохранитьДанныеToolStripMenuItem_MMS, выходToolStripMenuItem_MMS });
+            файлToolStripMenuItem_MMS.Name = "файлToolStripMenuItem_MMS";
+            файлToolStripMenuItem_MMS.Size = new Size(48, 20);
+            файлToolStripMenuItem_MMS.Text = "Файл";
+            // 
+            // справкаToolStripMenuItem_MMS
+            // 
+            справкаToolStripMenuItem_MMS.DropDownItems.AddRange(new ToolStripItem[] { оПрограммеToolStripMenuItem_MMS, руководствоПользователяToolStripMenuItem_MMS });
+            справкаToolStripMenuItem_MMS.Name = "справкаToolStripMenuItem_MMS";
+            справкаToolStripMenuItem_MMS.Size = new Size(65, 20);
+            справкаToolStripMenuItem_MMS.Text = "Справка";
+            // 
+            // загрузитьДанныеToolStripMenuItem_MMS
+            // 
+            загрузитьДанныеToolStripMenuItem_MMS.Name = "загрузитьДанныеToolStripMenuItem_MMS";
+            загрузитьДанныеToolStripMenuItem_MMS.Size = new Size(180, 22);
+            загрузитьДанныеToolStripMenuItem_MMS.Text = "Загрузить данные";
+            // 
+            // сохранитьДанныеToolStripMenuItem_MMS
+            // 
+            сохранитьДанныеToolStripMenuItem_MMS.Name = "сохранитьДанныеToolStripMenuItem_MMS";
+            сохранитьДанныеToolStripMenuItem_MMS.Size = new Size(180, 22);
+            сохранитьДанныеToolStripMenuItem_MMS.Text = "Сохранить данные";
+            // 
+            // выходToolStripMenuItem_MMS
+            // 
+            выходToolStripMenuItem_MMS.Name = "выходToolStripMenuItem_MMS";
+            выходToolStripMenuItem_MMS.Size = new Size(180, 22);
+            выходToolStripMenuItem_MMS.Text = "Выход";
+            // 
+            // оПрограммеToolStripMenuItem_MMS
+            // 
+            оПрограммеToolStripMenuItem_MMS.Name = "оПрограммеToolStripMenuItem_MMS";
+            оПрограммеToolStripMenuItem_MMS.Size = new Size(221, 22);
+            оПрограммеToolStripMenuItem_MMS.Text = "О программе";
+            // 
+            // руководствоПользователяToolStripMenuItem_MMS
+            // 
+            руководствоПользователяToolStripMenuItem_MMS.Name = "руководствоПользователяToolStripMenuItem_MMS";
+            руководствоПользователяToolStripMenuItem_MMS.Size = new Size(221, 22);
+            руководствоПользователяToolStripMenuItem_MMS.Text = "Руководство пользователя";
+            // 
+            // видToolStripMenuItem_MMS
+            // 
+            видToolStripMenuItem_MMS.DropDownItems.AddRange(new ToolStripItem[] { сменитьТемуToolStripMenuItem_MMS });
+            видToolStripMenuItem_MMS.Name = "видToolStripMenuItem_MMS";
+            видToolStripMenuItem_MMS.Size = new Size(39, 20);
+            видToolStripMenuItem_MMS.Text = "Вид";
+            // 
+            // сменитьТемуToolStripMenuItem_MMS
+            // 
+            сменитьТемуToolStripMenuItem_MMS.Name = "сменитьТемуToolStripMenuItem_MMS";
+            сменитьТемуToolStripMenuItem_MMS.Size = new Size(180, 22);
+            сменитьТемуToolStripMenuItem_MMS.Text = "Сменить тему";
+            // 
+            // FormMain
+            // 
+            AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Name = "Form1";
-            Text = "Form1";
+            ClientSize = new Size(1338, 674);
+            Controls.Add(tabControlMain_MMS);
+            Controls.Add(menuStripMain_MMS);
+            Font = new Font("Segoe UI Semibold", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            MainMenuStrip = menuStripMain_MMS;
+            Name = "FormMain";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Домоуправление";
             Load += Form1_Load;
+            tabControlMain_MMS.ResumeLayout(false);
+            menuStripMain_MMS.ResumeLayout(false);
+            menuStripMain_MMS.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TabControl tabControlMain_MMS;
+        private TabPage tabPageBase_MMS;
+        private TabPage tabPageStat_MMS;
+        private TabPage tabPageChart_MMS;
+        private TabPage tabPagePayInfo_MMS;
+        private TabPage tabPageAbout_MMS;
+        private MenuStrip menuStripMain_MMS;
+        private ToolStripMenuItem файлToolStripMenuItem_MMS;
+        private ToolStripMenuItem загрузитьДанныеToolStripMenuItem_MMS;
+        private ToolStripMenuItem сохранитьДанныеToolStripMenuItem_MMS;
+        private ToolStripMenuItem выходToolStripMenuItem_MMS;
+        private ToolStripMenuItem справкаToolStripMenuItem_MMS;
+        private ToolStripMenuItem оПрограммеToolStripMenuItem_MMS;
+        private ToolStripMenuItem руководствоПользователяToolStripMenuItem_MMS;
+        private ToolStripMenuItem видToolStripMenuItem_MMS;
+        private ToolStripMenuItem сменитьТемуToolStripMenuItem_MMS;
     }
 }
